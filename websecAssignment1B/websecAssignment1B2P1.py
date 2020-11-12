fe37d1ee282c29a214135b1be6997117e2d73b16
filipbergman <37209056+filipbergman@users.noreply.github.com>
2020-11-12 18:18:19 +0100

    Assignment 1B2 part 2 done

--- websecAssignment1B/websecAssignment1B2P1.py
+++ websecAssignment1B/websecAssignment1B2P1.py
@@ -4,17 +4,11 @@ import hashlib
 #Assignment 1B2 -----------------------------------------------------------------------------------------------------------------
 #Part 1
 print("Assignment 1B2 part 1")
-leaf = "2354cf006ef4eeefeddf29b9e68d5cb1918ed589"
-#Better solution than a long string is used in part 2
-node_str = "R69968f8d734080390646bd0f3afff78baadebd2bLed64e17870e63f55b71542f0818ff7639b1f9985L7a6ba60c80a893b7a02999b6415c6ec67d5883b4L64b64c7760e5559aefe701790ee0564af6458cb4L53f1eab7ccd09600908bc49044669cd8fc996171Rc5684eb22d8745a777037c19ff3eff85be800334L058e2c0d7d103a7b45b2a4408ac3389eb10048fe"
-node_length = 41
-node_list = [node_str[i:i+node_length] for i in range(0, len(node_str), node_length)]
-
-#Better solution
-with open('new.txt') as f:
+with open('leafAndNotes.txt') as f:
   node_list = f.readlines()
 node_list = [x.strip() for x in node_list] 
-leaf = node_list[0]
+leaf = node_list.pop(0)
+print(node_list)
 
 letter_list = []
 #Save and then remove the first character(R or L)
--- websecAssignment1B/websecAssignment1B2P2.py
+++ websecAssignment1B/websecAssignment1B2P2.py
@@ -15,12 +15,11 @@ depth_list = []
 depth_list.append(leaf_list)
 i = 0
 while len(depth_list[i]) > 1:
-  print(i)
   depth_list.append([])
   for k in range(int(len(depth_list[i]) / 2)):
     left_node = bytearray.fromhex(depth_list[i][k * 2])
     right_node = bytearray.fromhex(depth_list[i][k * 2 + 1])
-    print(depth_list[i][k * 2], " : ", depth_list[i][k * 2 + 1])
+    #print(depth_list[i][k * 2], " : ", depth_list[i][k * 2 + 1])
     
     concat_ba = left_node + right_node
 
@@ -29,13 +28,16 @@ while len(depth_list[i]) > 1:
 
   # Check if odd amount of nodes, if true, duplicate the last node and append to list on one lower depth
   if len(depth_list[i]) % 2 != 0:
+    print("ODD: ", len(depth_list[i]))
     node_to_duplicate = depth_list[i][len(depth_list[i]) - 1]
     depth_list[i].append(node_to_duplicate)
     depth_list[i + 1].append(hashlib.sha1(bytearray.fromhex(node_to_duplicate) + bytearray.fromhex(node_to_duplicate)).hexdigest())
-
+  
+  #print("LIST: ", depth_list[i])
+  print("LENGTH: ", len(depth_list[i]), "\n")
   i = i + 1
 
-print(depth_list)
+#print("DEPTH LIST: ", depth_list)
 
 #Put L/R in front of each node:
 for i in range(len(depth_list) - 1):
@@ -60,7 +62,8 @@ for i in range(len(depth_list) - 2):
   else:
     path_list.append(depth_list[i+1][next_index - 1])
 
-print("PATH LIST: ", path_list)
+#print("PATH LIST: ", path_list)
 
-final_concatenation = path_list[depth_val] + depth_list[len(depth_list)-1][0]
+final_concatenation = path_list[len(path_list) - depth_val] + depth_list[len(depth_list)-1][0]
 print(final_concatenation)
+#print("LENGTH: ", len(final_concatenation))
